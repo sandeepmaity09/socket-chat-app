ac902073a4de51e6db53bae3ee3af55ccb1c981a
sandeepmaity09 <sandeepmaity09@gmail.com>
2018-05-25 17:49:36 +0530

    index.html added

--- chat.js
+++ chat.js
@@ -14,8 +14,28 @@ let rooms = ['room1', 'room2', 'room3'];
 
 io.sockets.on('connection', function (socket) {
 
+    // socket.on('adduser', function (username) {
+
+    // })
+    // console.log(socket);
+
+    console.log(Object.keys(socket));
+    console.log('this is rooms', socket.rooms);
+
+    socket.on('join room', function (data) {
+        socket.join(data.room_id);
+        console.log('this is room', socket.room);
+        console.log('this is rooms', socket.rooms);
+    })
 
 
 
 
+})
+
+// app.listen(8080, function () {
+//     console.log('Server listening at 8080')
+// })
+server.listen(8080, () => {
+    console.log('server listening at 8080');
 })
